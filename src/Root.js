5f70776170668a100e2e09b05927ea8f92822070
Karolusrex <kalle.lundfall@gmail.com>
2019-04-22 13:35:00 +0200

    chore: remove irrelevant comment

--- src/Root.js
+++ src/Root.js
@@ -3,7 +3,6 @@ import EventsContainer from './pages/eventsContainer/EventsContainer';
 import './Root.css';
 
 const app = () => (
-  // TODO manage routes etc
   <div className="Root">
     <header className="RootHeader">
       <h1>Track Your Life Progress</h1>
